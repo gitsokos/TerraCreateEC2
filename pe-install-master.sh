3b5b8cf63f98d7d5637afdb6ea20826890102f73
G.Tsokos <grtsokos@gmail.com>
2023-10-22 13:25:48 +0300

    minor changes

--- pe-install-master.sh
+++ pe-install-master.sh
@@ -3,11 +3,7 @@
 echo '#!/bin/bash' > /tmp/keepalive
 echo 'while :; do echo -n -e "///////\v"; sleep 5; done' >> /tmp/keepalive
 chmod 0777 /tmp/keepalive
-cp /tmp/keeepalive /usr/bin
-
-sudo apt-get install git
-sudo apt-get install tree
-
+cp /tmp/keepalive /usr/bin
 
 PROGRESS=/tmp/pe-installation.progress
 
@@ -28,18 +24,13 @@ cd pupp*
 
 echo "Installer starting ..." >> $PROGRESS
 
-#echo $PATH >> $PROGRESS
-
-
 ./puppet-enterprise-installer -y -c conf.d/pe.conf
-echo "Install ok $?" >> $PROGRESS
+echo "Install ok($?)" >> $PROGRESS
 
-
-#echo $PATH >> $PROGRESS
 
 puppet infrastructure console_password --password adminp@ss
 
-echo "admin pass ok $?" >> $PROGRESS
+echo "admin pass ok($?)" >> $PROGRESS
 
 sudo echo "#!/bin/bash" > /etc/puppetlabs/puppet/autosign.conf
 sudo echo "echo -n \`date\`: >> /tmp/autosign.log" >> /etc/puppetlabs/puppet/autosign.conf
@@ -50,20 +41,17 @@ sudo chmod 0777 /etc/puppetlabs/puppet/autosign.conf
 
 echo Autosign >> $PROGRESS
 
-#sudo echo "" >>  /etc/puppetlabs/puppet/puppet.conf
-#sudo echo "[server]" >>  /etc/puppetlabs/puppet/puppet.conf
-#sudo echo "autosign=/etc/puppetlabs/puppet/autosign.conf" >>  /etc/puppetlabs/puppet/puppet.conf
 puppet config --section master set autosign /etc/puppetlabs/puppet/autosign.conf
 
 echo Puppet-conf >> $PROGRESS
 
 puppet agent -t --logdest /tmp/agent-log1
 
-echo "agent phase 1 ok $?" >> $PROGRESS
+echo "agent phase 1 ok($?)" >> $PROGRESS
 
 puppet agent -t --logdest /tmp/agent-log2
 
-echo "agent phase 2 ok $?" >> $PROGRESS
+echo "agent phase 2 ok($?)" >> $PROGRESS
 
 
 runinterval=25s
@@ -73,17 +61,3 @@ echo "set runinterval=$runinterval" >> $PROGRESS
 
 echo "installation complete" >> $PROGRESS
 
-
-echo '#!/bin/bash' > /tmp/keepalive
-echo 'while :; do echo -n -e "///////\v"; sleep 5; done' >> /tmp/keepalive
-chmod 0777 /tmp/keepalive
-
-#echo $PATH >> $PROGRESS
-
-#echo "export PATH=\$PATH:/opt/puppetlabs/server/bin" > /tmp/setpath
-
-#source /tmp/setpath
-
-#echo $PATH >> $PROGRESS
-
-#
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 nodes = 0
 w_nodes = 0
-user_data_replace_on_change_master = false
+user_data_replace_on_change_master = true # false
 user_data_replace_on_change_node = false
 user_data_replace_on_change_w_node = true # false
