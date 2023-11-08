cc337b089c2d6194275f30c7a9e3eb202b1b5b45
G.Tsokos <grtsokos@gmail.com>
2023-11-08 16:08:54 +0200

    refacoring vars

--- main.tf
+++ main.tf
@@ -10,39 +10,18 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable "master_name" {
-  type		= string
-  default	= "master"	
-}
-
-variable "u_node_name" {
-  type		= string
-  default	= "ubuntu"	
-}
-variable "r_node_name" {
-  type		= string
-  default	= "redhat"	
-}
-
-variable "w_node_name" {
-  type		= string
-  default	= "wnode"	
-}
-
-variable "u_nodes" {
-  type		= number
-  default	= 0
-}
-
-variable "r_nodes" {
-  type		= number
-  default	= 0
-}
-
-variable "w_nodes" {
-  type		= number
-  default	= 0
-}
+variable "az" {}
+variable "master_ip" {}
+variable "u_node_ip_prefix" {}
+variable "r_node_ip_prefix" {}
+variable "w_node_ip_prefix" {}
+variable "master_name" {}
+variable "u_node_name" {}
+variable "r_node_name" {}
+variable "w_node_name" {}
+variable "u_nodes" { }
+variable "r_nodes" {}
+variable "w_nodes" {}
 
 variable "master_ami" {
   type    = string
@@ -50,10 +29,8 @@ variable "master_ami" {
 }
 
 variable "master_type" {
-  type		= string
-  default	= "t2.large"
-#  default	= "t2.xlarge"
-#  default	= "t2.2xlarge"
+  type    = string
+  default = "t2.large" #  t2.xlarge t2.2xlarge
 }
 
 variable "u_node_ami" {
@@ -67,8 +44,8 @@ variable "r_node_ami" {
 }
 
 variable "node_type" {
-  type		= string
-  default	= "t2.small"
+  type    = string
+  default = "t2.small"
 }
 
 variable "w_node_ami" {
@@ -77,14 +54,9 @@ variable "w_node_ami" {
 }
 
 variable "w_node_type" {
-  type		= string
-  default	= "t2.small"
+  type    = string
+  default = "t2.small"
 }
-
-#variable "inventory" {
-#  type		= string
-#  default	= "inventoryx.ini"
-#}
 
 variable "keyname" {
   type    = string
@@ -96,27 +68,13 @@ variable "w_keyname" {
   default = "ec2wid_rsa"
 }
 
-variable "user_data_replace_on_change_master" {
-  type = bool
-  default = false
-}
-
-variable "user_data_replace_on_change_u_node" {
-  type = bool
-  default = false
-}
-variable "user_data_replace_on_change_r_node" {
-  type = bool
-  default = false
-}
-
-variable "user_data_replace_on_change_w_node" {
-  type = bool
-  default = false
-}
+variable "user_data_replace_on_change_master" {}
+variable "user_data_replace_on_change_u_node" {}
+variable "user_data_replace_on_change_r_node" {}
+variable "user_data_replace_on_change_w_node" {}
 
 resource "aws_instance" "master" {
-  ami           = var.master_ami # "ami-0c6ebbd55ab05f070"
+  ami           = var.master_ami
   instance_type = var.master_type
 
   tags = {
@@ -127,43 +85,36 @@ resource "aws_instance" "master" {
 
   vpc_security_group_ids = [aws_security_group.main.id]
 
-  availability_zone = "eu-west-3a"
-  private_ip ="172.31.13.0"
+  availability_zone = var.az
+  private_ip        = var.master_ip # "172.31.13.0"
 
   provisioner "local-exec" {
     command = "echo \"The server's IP address is ${self.private_ip}\""
   }
 
   user_data = templatefile("pe-install-master.sh",{})
-
-  user_data_replace_on_change = var.user_data_replace_on_change_master # true 
+  user_data_replace_on_change = var.user_data_replace_on_change_master 
 }
 
 resource "aws_instance" "u_nodes" {
-  ami           = var.u_node_ami # "ami-0c6ebbd55ab05f070"
+  ami           = var.u_node_ami
   instance_type = var.node_type
   count         = var.u_nodes
   tags = {
     Name = "${var.u_node_name}-${count.index+1}"
   }
 
-  availability_zone = "eu-west-3a"
-  private_ip = "172.31.13.10${count.index+1}"
+  availability_zone = var.az
+  private_ip        = "${var.u_node_ip_prefix}${count.index+1}"
 
   key_name = var.keyname
 
   vpc_security_group_ids = [aws_security_group.main.id]
 
-#  provisioner "remote-exec" {
-#    inline = ["echo \"The server's IP address is ${self.private_ip}\" > /tmp/message"]
-#  }
-
   user_data = templatefile("pe-install-node.sh",{master_private_dns=aws_instance.master.private_dns,node_os=var.u_node_name })
   user_data_replace_on_change = var.user_data_replace_on_change_u_node # true 
 
-  depends_on = [
-    aws_instance.master
-  ]
+  depends_on = [aws_instance.master]
 
 # connection {
 #   type = "ssh"
@@ -188,16 +139,13 @@ resource "aws_instance" "r_nodes" {
 
   vpc_security_group_ids = [aws_security_group.main.id]
 
-
-  availability_zone = "eu-west-3a"
-  private_ip = "172.31.13.15${count.index+1}"
+  availability_zone = var.az
+  private_ip        = "${var.r_node_ip_prefix}${count.index+1}"
 
   user_data = templatefile("pe-install-node.sh",{master_private_dns=aws_instance.master.private_dns,node_os=var.r_node_name })
   user_data_replace_on_change = var.user_data_replace_on_change_r_node # true 
 
-  depends_on = [
-    aws_instance.master
-  ]
+  depends_on = [aws_instance.master]
 
 }
 
@@ -216,16 +164,17 @@ resource "aws_instance" "w_nodes" {
 
   vpc_security_group_ids = [aws_security_group.main.id]
 
+  availability_zone = var.az
+  private_ip        = "${var.w_node_ip_prefix}${count.index+1}"
+
 #  key_name = "winkey"
 #  vpc_security_group_ids = ["sg-23847238474f"]
 #  subnet_id     = "subnet-2348273423"
 
-  user_data = templatefile("pe-install-wnode.ps1",{master_private_dns=aws_instance.master.private_dns }) # "pe-install-wnode.ps1"
-  user_data_replace_on_change = var.user_data_replace_on_change_w_node # true 
+  user_data = templatefile("pe-install-wnode.ps1",{master_private_dns=aws_instance.master.private_dns })
+  user_data_replace_on_change = var.user_data_replace_on_change_w_node
 
-  depends_on = [
-    aws_instance.master
-  ]
+  depends_on = [aws_instance.master]
 
 }
 
@@ -274,12 +223,12 @@ resource "aws_security_group" "main" {
 }
 
 
-resource "aws_key_pair" "key_pair" { # "deployer" {
+resource "aws_key_pair" "key_pair" {
   key_name = "${var.keyname}"  # "ec2id_rsa"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNQRbdwoOjhTcgagF+owTymeiWsrQrpQ1and7MJIUq//kG/B24qUhRo+gbsH3Bt2sFYzO/IU9wiwfwUd2WrzX9WzpN2wSsppPHZnfpsDpp9UYI3CIvQensc2RIMias//KdjAuG+oHFnSJODo9RtDZ4MXvqgQVrIVYERUV48rTAQFUc+YmXHAXsnBeNtc3RIh1J/OC4TxO82F8cRbIrMKPgrXDylxQGFV1NdOtS+PfDRAYwPyJRbdRTqDOjInllH68cetchJbB9vxfBEzMF78E7EH6NC0b6GHNSXVxJfiyO5xJPO1TZnOupTRVYSvk2yIwjrCBen5tGY3oKYOSnEV1EVMc3WOR2DhR2IY//z6JLxQmvH9WlQyCnzTT/waYQhbGFoD5CXjYlm2eM/bcd4XJXg6SFNT2ewWKrN5huefZ+Cl1obQB+5gG+wYaKMXwD8hNROv3nWWUYV9nAX1RWtQBD9cYoudBsCPkwBbUm1sXnEe97oFtOvHD/ox3QFg/qrGs= grtso@LAPTOP-2N9STEIV"
 }
 
-resource "aws_key_pair" "w_key_pair" { # "deployer" {
+resource "aws_key_pair" "w_key_pair" {
   key_name = "${var.w_keyname}"  # "ec2idw_rsa"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzMO/pgggZx55/d8hM7ZeMCqHWdkhssH5Kept65ikeP+4YD5r2WLcbkpHFiJHTYraSOkjR7Ojl+b11w1ciooRveG1VUhHc6AyFRIrz95+vPv/rdyxfJg1XQsMbSfsipDxomXYySqRPdaBw67UM7X79z9qoWG03hob652QOQPQe1KlgUhuZ/kIgzuTV50Q2z2CtTiy6j79x7ndINZaS+xN1Oq+3h97MnFcgFxgLubAAaInj9nECeMVhBe1G9AY1Ot7ehpfX/Skra9GnkNFQfv+K7sqPZOAsAox8czVy3Wh2I6mMG6br9E+Su8Yqy6UNFtGPmrBZdZXLyGoaXfms+6e/YH+KsGjeSgLQyt/FXTdPpnYbDxGa6rhjRBeasIsN6a+5icZ4gp+bGqT4OOPn2hSua+hnRbx0AXCv/b6GEvt3likpXGP3w0Dq3vDFPgMRs1BAiil5XgJ6U89bx54+4ARAiaLfSgkFKP1KLo9xc7BBIceBIwwmmBPcrbuXOaC931c= george@DESKTOP-74M7RJ1"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,20 @@
+u_nodes = 0
+r_nodes = 0
+w_nodes = 0
+user_data_replace_on_change_master = true # false
+user_data_replace_on_change_u_node = true # false
+user_data_replace_on_change_r_node = true # false
+user_data_replace_on_change_w_node = true # false
+#
+az = "eu-west-3a"
+
+master_ip        = "172.31.13.0"
+u_node_ip_prefix = "172.31.13.10"
+r_node_ip_prefix = "172.31.13.15"
+w_node_ip_prefix = "172.31.13.20"
+
+master_name = "master"
+u_node_name = "ubuntu"
+r_node_name = "redhat"
+w_node_name = "window"
+
