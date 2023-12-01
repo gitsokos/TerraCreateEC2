b1adeb24ba5b9212cb914e9af210fd50e94461ad
G.Tsokos <grtsokos@gmail.com>
2023-12-01 13:15:20 +0000

    change keepalive

--- pe-install-node.sh
+++ pe-install-node.sh
@@ -4,7 +4,8 @@ PROGRESS=/tmp/pa-installation.progress
 pwd >> $PROGRESS
 
 echo '#!/bin/bash' > /tmp/keepalive
-echo 'while :; do echo -n -e "///////\v"; sleep 5; done' >> /tmp/keepalive
+echo "trap 'echo; exit 0;' INT" >> /tmp/keepalive
+echo 'while :; do for x in {1..50}; do printf "\033[1C"; sleep 2; done; for x in {1..50}; do printf "\033[1D"; sleep 2; done done' >> /tmp/keepalive
 chmod 0777 /tmp/keepalive
 sudo cp /tmp/keepalive /usr/bin
 
