2ee06bf0b4cafc35d164d49ed6e90d558bcd1f6f
G.Tsokos <grtsokos@gmail.com>
2023-12-07 10:20:40 +0200

    delete puppet specific code and files

--- pe-install-wnode.ps1
+++
@@ -1,5 +0,0 @@
-<powershell>
-New-Item \Users\Administrator\Desktop\start.txt  -ItemType file
-[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://${master_private_dns}:8140/packages/current/install.ps1', 'install.ps1'); .\install.ps1 -v
-New-Item \Users\Administrator\Desktop\end.txt  -ItemType file
-</powershell>
