28b178f47b21f3c219ad2d5808b3abe2c74dddd3
G.Tsokos <grtsokos@gmail.com>
2023-12-13 15:00:19 +0200

    upgrade git and nodejs

--- install-progs.sh
+++ install-progs.sh
@@ -7,3 +7,17 @@ echo 'while :; do for x in {1..50}; do printf "\033[1C"; sleep 2; done; for x in
 chmod 0777 /tmp/keepalive
 sudo cp /tmp/keepalive /usr/bin
 
+
+# prepare git installation
+add-apt-repository --yes ppa:git-core/ppa
+
+# prepare nodejs installation
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+# install
+apt-get update
+apt-get -yqq install git
+apt-get -yqq install nodejs
+
