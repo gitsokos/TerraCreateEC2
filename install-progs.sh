091b5d485a09c333814e677e1672a214feaf05a0
G.Tsokos <grtsokos@gmail.com>
2023-12-16 17:45:15 +0200

    tree added

--- install-progs.sh
+++ install-progs.sh
@@ -33,3 +33,4 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y tree
