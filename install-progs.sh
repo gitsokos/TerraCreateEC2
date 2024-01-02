eb2ae30713d09f48692bd945f56f1f34207594fa
G.Tsokos <grtsokos@gmail.com>
2024-01-02 17:58:46 +0200

    install minor changes

--- install-progs.sh
+++ install-progs.sh
@@ -18,33 +18,33 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 echo "------------------------------------------- Install git ----------------------------------------------------------------"
-apt-get update
+apt-get -yqq update
 apt-get -yqq install git
 git config --system credential.helper store
 echo "------------------------------------------- Install nodejs -------------------------------------------------------------"
 apt-get -yqq install nodejs
 
-echo "------------------------------------------- Add Docker's official GPG key ----------------------------------------------"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+echo "------------------------------------------- Install docker -------------------------------------------------------------"
+sudo apt-get -yqq update
+sudo apt-get -yqq install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo "------------------------------------------- Add the repository to Apt sources ------------------------------------------"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get -yqq update
 
-echo "------------------------------------------- Install docker -------------------------------------------------------------"
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt-get install -y tree
+sudo apt-get -yqq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "------------------------------------------- Install tree ---------------------------------------------------------------"
+sudo apt-get -yqq install tree
 
 echo "------------------------------------------- Install xrdp ---------------------------------------------------------------"
-sudo apt install -y xrdp
+sudo apt-get -yqq install xrdp
 sudo systemctl enable xrdp
 echo "------------------------------------------- Install gnome --------------------------------------------------------------"
-sudo add-apt-repository -y ppa:gnome3-team/gnome3
-sudo apt-get install -y gnome-shell ubuntu-gnome-desktop
+sudo add-apt-repository --yes ppa:gnome3-team/gnome3
+sudo apt-get -yqq install gnome-shell ubuntu-gnome-desktop
 
 echo "------------------------------------------- Set passwd for ubuntu ------------------------------------------------------"
 echo ubuntu:ubuntu000 | sudo chpasswd
@@ -65,18 +65,18 @@ sudo GOPATH=/usr/local/go /usr/local/go/bin/go install sigs.k8s.io/kind@v0.20.0
 echo "Install kind: "$?
 
 echo "-------------------------------------------- Install kubectl -----------------------------------------------------------"
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get -yqq update
+sudo apt-get -yqq install apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt-get -yqq update
+sudo apt-get -yqq install kubectl
 
 echo "-------------------------------------------- Install terraform ---------------------------------------------------------"
 wget -O - https://apt.releases.hashicorp.com/gpg 2>/dev/null | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update || true
-sudo apt install -y terraform
+sudo apt-get -yqq update || true
+sudo apt-get -yqq install terraform
 
 echo "-------------------------------------------- Install azure-cli ---------------------------------------------------------"
-sudo apt install -y azure-cli
+sudo apt-get -yqq install azure-cli
