051ff3595571476336854fe1996ec48fdaf451bb
G.Tsokos <grtsokos@gmail.com>
2024-01-09 17:03:33 +0200

    create inventory.ini for ansible

--- inventory.tf
+++ inventory.tf
@@ -2,12 +2,35 @@
 resource "local_file" "inventory" {
   filename = "inventory.ini"
   file_permission = "0664"
-  content = <<-EOT
-    [nodes]
-    ${length(module.master)}
-    %{for v in module.master ~}
-${v.public_ip}
-    %{ endfor ~}
+  content = <<EOT
+    %{~if length(module.master)>0 ~}
+      [master]
+      %{~for v in module.master ~}
+      ${v.public_ip}
+      %{~endfor ~}
+    %{~endif ~}
+
+    %{~if length(module.unodes)>0 ~}
+      [unodes]
+      %{~for v in module.unodes ~}
+      ${v.public_ip}
+      %{~ endfor ~}
+    %{~endif ~}
+
+    %{~if length(module.rnodes)>0 ~}
+      [rnodes]
+      %{~for v in module.rnodes ~}
+      ${v.public_ip}
+      %{~ endfor ~}
+    %{~endif ~}
+
+    %{~if length(module.wnodes)>0 ~}
+      [wnodes]
+      %{~for v in module.wnodes ~}
+      ${v.public_ip}
+      %{~ endfor ~}
+    %{~endif ~}
+
   EOT
 }
 
