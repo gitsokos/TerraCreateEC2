6e7c540f9d1e0ffdf8073db4d4c922df1f628752
G.Tsokos <grtsokos@gmail.com>
2024-01-09 18:31:08 +0200

    fix azure-cli installation

--- install-progs.sh
+++ install-progs.sh
@@ -83,4 +83,14 @@ sudo apt-get -yqq update || true
 sudo apt-get -yqq install terraform
 
 echo "-------------------------------------------- Install azure-cli ---------------------------------------------------------"
+sudo apt-get -yqq update
+sudo apt-get -yqq install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor |    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+AZ_DIST=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+echo AZ_DIST-$AZ_DIST
+
+sudo apt-get -yqq update
 sudo apt-get -yqq install azure-cli
